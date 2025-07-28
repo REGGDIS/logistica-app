89b9d053db36aff3152a55e97c2832646c631b5f
REGGDIS <regdis@gmail.com>
2025-07-28 17:01:27 -0400

    Proyecto base: Spring Boot Logística

---
+++ src/main/java/com/regdevs/logistica/LogisticaAppApplication.java
@@ -0,0 +1,13 @@
+package com.regdevs.logistica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/regdevs/logistica/LogisticaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.regdevs.logistica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
