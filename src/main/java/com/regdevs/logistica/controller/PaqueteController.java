44ffc5ba1a3b579b2b83a73a7f5ef2c749644670
REGGDIS <regdis@gmail.com>
2025-07-29 16:43:13 -0400

    Agregar vista /paquetes y controlador para listar paquetes con Bootstrap

---
+++ src/main/java/com/regdevs/logistica/controller/PaqueteController.java
@@ -0,0 +1,24 @@
+package com.regdevs.logistica.controller;
+
+import com.regdevs.logistica.model.Paquete;
+import com.regdevs.logistica.service.PaqueteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class PaqueteController {
+
+    @Autowired
+    private PaqueteService paqueteService;
+
+    @GetMapping("/paquetes")
+    public String listarPaquetes(Model model) {
+        List<Paquete> listaPaquetes = paqueteService.listarTodos();
+        model.addAttribute("paquetes", listaPaquetes);
+        return "paquetes"; // Nombre del archivo .html en /templates
+    }
+}
